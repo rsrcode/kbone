ea327f4f2a394e35f7c77b21e92f21799ad103a8
xgwang <xgwang@sysnew.com>
2020-12-11 16:30:32 +0800

    demo0

---
+++ examples/demo0/src/index/main.js
@@ -0,0 +1,14 @@
+import a from './a'
+import b from './b'
+
+// rem 和页面样式修改
+window.onload = function () {
+    document.documentElement.style.fontSize = document.documentElement.getBoundingClientRect().width / 16 + 'px'
+    document.documentElement.style.backgroundColor = '#fffbe7'
+}
+window.onerror = (message, source, lineno, colno, error) => {
+    console.log('window.onerror => ', message, source, lineno, colno, error)
+};
+window.addEventListener('error', evt => console.log('window.addEventListener(\'error\') =>', evt))
+
+
